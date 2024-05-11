08774e66a35c58f5d4c7fae7a2dad5bd48826077
SenySunny <senysunny@163.com>
2024-05-11 18:26:19 +0800

    Site updated: 2024-05-11 18:26:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Web/hexo博客使用的一些常见问题（一）：外站图片显示，站内链接，嵌入B站视频/","Embedded/ESP32学习笔记（一）：ESP-IDF配合VSCode的一些方法和技巧/","Web/hexo博客搭建（一）：在PC上搭建Hexo博客系统，并设置博客主题/","Web/hexo博客搭建（三）：部署云端Linux服务器，搭建宝塔Linux面板，实现远程或域名访问/","Web/hexo博客搭建（四）：本地Linux系统部署，搭建宝塔Linux面板，通过内网穿透实现域名访问/","Web/hexo博客搭建（二）：把hexo博客部署到github网页托管，实现通过类似xxx-github-io来访问博客/"];function toRandomPost(){
+var posts=["Embedded/ESP32学习笔记（一）：ESP-IDF配合VSCode的一些方法和技巧/","Web/hexo博客使用的一些常见问题（一）：外站图片显示，站内链接，嵌入B站视频/","Web/hexo博客搭建（一）：在PC上搭建Hexo博客系统，并设置博客主题/","Web/hexo博客搭建（二）：把hexo博客部署到github网页托管，实现通过类似xxx-github-io来访问博客/","Web/hexo博客搭建（三）：部署云端Linux服务器，搭建宝塔Linux面板，实现远程或域名访问/","Web/hexo博客搭建（四）：本地Linux系统部署，搭建宝塔Linux面板，通过内网穿透实现域名访问/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
